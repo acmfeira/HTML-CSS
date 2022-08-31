36d0f20de6855c5981ebc8148e3982a8e20affc4
acmfeira <acmfeira@gmail.com>
2022-08-31 13:31:42 -0300

    ex026 - Login
    add jquery, php and db SQLite3

---
+++ modulo-4/ex027/db.php
@@ -0,0 +1,37 @@
+<?php
+
+    class mydb extends SQLite3 {
+
+        function __construct(){
+            $this->open('./login.db');
+        }
+
+        function addUser($user,$pass){
+            return $this->exec("insert into UserLogin (name,password) VALUES ('$user','$pass')");
+        }
+
+        function close(){
+            $this->close();
+        }
+    }
+    
+    $conn = new mydb();
+    
+    if($conn){
+            
+        $user = $_POST['userName'];
+        $pass = $_POST['password'];
+        
+        $status = $conn->addUser($user,$pass) ? 'success' : 'error';
+        
+        print_r($status);
+    }
+
+    $conn->close();
+    /*
+    $res = $db.exec('select * from UserLogin');
+    var_dump($res->fetchArray());
+    https://www.php.net/manual/en/sqlite3.open.php
+    */
+
+?>
